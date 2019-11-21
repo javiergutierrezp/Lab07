1883bf71cdada7997b14feea9db0ade351db8d6f
javiergutierrezp <javiergutzp@hotmail.com>
2019-11-21 12:38:00 -0600

    Add files via upload

--- Lab07/src/routes.js
+++ Lab07/src/routes.js
@@ -3,10 +3,10 @@ const wizards = require('./controllers/wizards')
 
 const router = express.Router()
 
-router.get('/wizards', wizards.getUsers)
-router.get('/wizards/:id', wizards.getUser)
-router.post('/wizards', wizards.createUser)
-router.patch('/wizards/:id', wizards.updateUser)
-router.delete('/wizards/:id', wizards.deleteUser)
+router.get('/wizards', wizards.getWizards)
+router.get('/wizards/:id', wizards.getWizard)
+router.post('/wizards', wizards.createWizard)
+router.patch('/wizards/:id', wizards.updateWizard)
+router.delete('/wizards/:id', wizards.deleteWizard)
 
 module.exports = router
